78d17d783d31259e94deedbd08ec89ba6a3a527e
kmirek1 <karissa.mirek@gmail.com>
2025-11-05 14:41:19 -0600

    Update week5_bellRinger.py

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -1,38 +1,66 @@
 # Problem Set 1: Indexing and Slicing Strings
 # Basic Indexing:
-# Given the string magic = 'abracadabra',
+magic = 'abracadabra'
 # a. Retrieve the 5th character.
+print(magic[4])
 # b. Retrieve the second to last character.
+print(magic[9])
 # c. Find the first occurrence of the letter 'c'.
+print(magic[4])
 
 # Advanced Slicing:
-# Given the string alphabet = 'abcdefghijklmnopqrstuvwxyz',
+# Given the string 
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 # a. Extract the letters 'hij'.
+print(alphabet[7:10])
 # b. Extract every second letter starting from 'a' to 'm'.
+print(alphabet[0:13:2])
 # c. Reverse the entire string using slicing.
-
+print(alphabet[::-1])
 # Problem Set 2: Extracting Information
 # From Descriptions:
-# Extract the name of the famous personality from the quote "Ask not what your country can do for you — ask what you can do for your country. - John F. Kennedy"
+# Extract the name of the famous personality from the 
+quote = "Ask not what your country can do for you — ask what you can do for your country. - John F. Kennedy"
+print(quote.find('John F. Kennedy'))
+personality_name = quote[83:]
+print(personality_name)
 
 # Manipulating Words:
-# Given the string info = "Python is fun. Fun is good. Good is subjective.",
+# Given the string info 
+quote2 = "Python is fun. Fun is good. Good is subjective."
 # a. Extract the word 'subjective' without knowing its exact position.
+print(quote2.find('subjective'))
+subjective = quote2[36:]
+print(subjective)
 # b. Extract every third word.
+third_word = quote2.split()[::3]
+print(third_word)
 # c. Reverse the positions of the words, but keep the characters in each word in the same order.
+reversed = quote2.split()[::-1]
+reversed = ''.join(reversed)
+print(reversed)
 
 # Problem Set 3: String Methods
 # Upper & Lower:
-# Convert the following text to lowercase: "MAY THE FORCE BE WITH YOU."
+# Convert the following text to lowercase: 
+quote3 = "MAY THE FORCE BE WITH YOU."
+print(quote3.lower())
 
 # String Joining and Splitting:
-# Given the list motto = ["Make", "haste", "slowly."],
+# Given the list 
+motto = ["Make", "haste", "slowly."]
 # a. Convert the list into a single string.
+single_string =''.join(motto)
+print(single_string)
 # b. Now, split the string at every occurrence of the letter 'a'.
+split_motto =single_string.split('a')
+print(split_motto)
 
 # Replacing Words:
-# Modify the sentence: "Life is what happens when you are busy making other plans."
+# Modify the sentence: 
+sentence =  "Life is what happens when you are busy making other plans."
 # a. Replace "busy" with "distracted".
+
 # b. Replace "plans" with "mistakes".
 
 # Problem Set 4: String Properties and Advanced Operations
