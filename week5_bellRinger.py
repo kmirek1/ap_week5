1f5fe79c0a2a9f7582cdcae2bc19486bc4907a3a
kmirek1 <karissa.mirek@gmail.com>
2025-11-05 14:57:02 -0600

    Update week5_bellRinger.py

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -82,6 +82,10 @@ else:
   print("The word 'moonlight' does not appear in the quote.")
 
 # Length and Count:
-# a. Calculate the number of characters (including spaces and punctuation) in the word/phrase: "Supercalifragilisticexpialidocious".
-
+# a. Calculate the number of characters (including spaces and punctuation) in the word/phrase: 
+phrase ="Supercalifragilisticexpialidocious"
+length_phrase = len(phrase)
+print(length_phrase)
+count_i = phrase.count('i')
+print(count_i)
 # b. Count the number of times the letter 'i' appears in the same word/phrase.
